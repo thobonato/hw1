937e07db195f5e00111ac27c7dae301b8e45cbfe
Thomaz Bonato <bonato@usc.edu>
2025-02-01 04:16:53 +0000

    pushing code with answer to #2 at 97.14

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,47 @@
 
 int main(int argc, char* argv[])
 {
+  std::cout << "running" << std::endl;
+  ULListStr list = ULListStr();
+  
+  // insert front threeitemadd
+  // Value of: checkListContent(populatedList, contents)
+  //   Actual: false (List content mismatch: Expected value at index 0 was 768, but element was actually 
+  // Expected value at index 1 was 1024, but element was actually 
+  // Expected value at index 2 was 1536, but element was actually 
+  // )
+  // Expected: true
+
+  
+  // insert back then front
+  // grade_ulliststr.cpp:205: Failure
+  // Value of: checkListContent(&list, {"fred", "bob"})
+  //   Actual: false (List content mismatch: Expected value at index 0 was fred, but element was actually 
+  // Expected value at index 1 was bob, but element was actually 
+  // )
+
+  list.push_back("bob");
+	list.push_front("fred");
+
+  
+
+  // head3 elements
+  // "foo", "BAR", "baz"
+  // pop_front();
+
+  // ListRemove.SongOfBackAndFront
+  // tried to free something impossible
+  // Value of: checkListContent(&list, {"newback"})
+  //   Actual: false (List content mismatch: Expected value at index 0 was newback, but element was actually 
+  // )
+
+  std::cout << "list.back()" << list.back() << std::endl;
+  std::cout << "list.front()" << list.front() << std::endl;
+
+  int sz = list.size();
+  std::cout << "list size " << sz << std::endl;
+  for(size_t index = 0; index < sz; ++index){
+    std::cout << list.get(index) << std::endl;
+  }
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
 
 ULListStr::ULListStr()
 {
@@ -25,6 +26,142 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val){
+  // case 0: empty list (head_ and tail_ == null)
+  // put it at the front of the array, choose whatever you'd like
+  if (this->head_ == NULL){
+    Item* item = new Item();
+    item->val[0] = val;
+    item->last = 0;
+    item->last = 1;
+    this->head_ = item;
+    this->tail_ = item;
+  }
+  else if(this->tail_->last < ARRSIZE) { // case 1: there is space
+    this->tail_->val[this->tail_->last] = val;
+    this->tail_->last++;
+  } 
+  else{ // case 2: there is no space, allocate new Item(), add it to the start that new Item's array
+    Item* item = new Item();
+    
+    // set up item correctly
+    item->val[0] = val;
+    item->first = 0;
+    item->last = 1;
+    item->prev = this->tail_;
+
+    // place it in the linked list
+    this->tail_->next = item;
+    this->tail_ = item;
+  }
+  this->size_++;
+
+}
+
+void ULListStr::pop_back(){
+  if (this->size_ < 1){
+    return;
+  }
+
+  int diff = this->tail_->last - this->tail_->first;
+  if (diff > 1){ // case 1: pop front without having to deallocate an item
+    this->tail_->last--;
+  } 
+  else{ // case 2: pop front having to deallocate an item
+    Item* tmp = this->tail_;
+    this->tail_ = this->tail_->prev;
+    
+    // if popping front will erase whole array, 
+    if (this->tail_ == nullptr){
+      this->head_ = nullptr;
+    }
+    delete tmp;
+  }
+  this->size_--;
+}
+
+void ULListStr::push_front(const std::string& val){
+  // case 0: empty list (head_ and tail_ == null)
+  // put it at the front of the array, choose whatever you'd like
+  if (this->head_ == NULL){
+    Item* item = new Item();
+    item->val[0] = val;
+    item->last = 0;
+    item->last = 1;
+    this->head_ = item;
+    this->tail_ = item;
+  }
+  else if(this->head_->first > 0) { // case 1: there is space
+    this->head_->val[this->head_->first - 1] = val;
+    this->head_->first--;
+  } 
+  else{ // case 2: there is no space, allocate new Item(), add it to the back that new Item's array
+    Item* item = new Item();
+    
+    // set up item correctly
+    item->val[ARRSIZE - 1] = val;
+    item->first = ARRSIZE - 1;
+    item->last = ARRSIZE;
+    item->next = this->head_;
+
+    // place it in the linked list
+    this->head_->prev = item;
+    this->head_ = item;
+  }
+  this->size_++;
+}
+
+void ULListStr::pop_front(){
+  if (this->size_ < 1){
+    return;
+  }
+
+  int diff = this->head_->last - this->head_->first;
+  if (diff > 1){ // case 1: pop front without having to deallocate an item
+    this->head_->first++;
+  } 
+  else{ // case 2: pop front having to deallocate an item
+    Item* tmp = this->head_;
+    this->head_ = this->head_->next;
+    
+    // if popping front will erase whole array, 
+    if (this->head_ == nullptr){
+      this->tail_ = nullptr;
+    }
+    delete tmp;
+  }
+  this->size_--;
+}
+
+std::string const & ULListStr::back() const{
+  return this->tail_->val[this->tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const{
+  return this->head_->val[this->head_->first];
+}
+
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  Item* item = this->head_;
+
+
+  // go to right item
+  while (item != nullptr && item->first + loc > ARRSIZE - 1){
+    loc = loc - item->last + item->first;
+    item = item->next;
+  }
+
+  // cases when null
+  if (item == nullptr || item->val == NULL){
+    return NULL;
+  }
+  // return item
+  return &item->val[item->first + loc];
+}
+
+
+// CODE WRITTEN ABOVE HERE
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
